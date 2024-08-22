82f29fe950fd9e675dc88d59051fd1354df66616
unknown <aubaimahmmoud@example.com>
2024-08-22 11:03:41 +0300

    ev company

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+import './App.css';
+import React, { } from 'react';
+import Quiz from './Components/Quiz/Quiz.jsx'
+
+function App() {
+  return (
+    <div>
+      <Quiz />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/assets/data.js
@@ -0,0 +1,41 @@
+export const data = [
+    {
+        question: "Which device is required for the Internet connection? ",
+        option1: "Modem",
+        option2: "Router",
+        option3: "LAN Drive",
+        option4: "Pen Drive",
+        ans: 1,
+    },
+    {
+        question: "Which content has the highest number of countries? ",
+        option1: "Asia",
+        option2: "Eroupe",
+        option3: "North Amreca",
+        option4: "Africa",
+        ans: 4,
+    },
+    {
+        question: "Junk e-mail is also called? ",
+        option1: "Spam",
+        option2: "Fake",
+        option3: "Archive",
+        option4: "Bin",
+        ans: 1,
+    }, {
+        question: "A copmuter cannot BOOT if it does not have the? ",
+        option1: "Application SoftWare",
+        option2: "Internet",
+        option3: "Operating System",
+        option4: "Mouse",
+        ans: 3,
+    }, {
+        question: "First page of Website is termed as? ",
+        option1: "Index Page",
+        option2: "Home Page",
+        option3: "Site Page",
+        option4: "Nothing",
+        ans: 2,
+    },
+]
+
